67f7dbac368437725f3e113c410aedc13aba2624
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-08-12 23:18:37 -0300

    first commit

---
+++ src/main/java/br/edu/projeto_integrado/somar/SomarAppApplication.java
@@ -0,0 +1,13 @@
+package br.edu.projeto_integrado.somar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/projeto_integrado/somar/SomarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.projeto_integrado.somar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
