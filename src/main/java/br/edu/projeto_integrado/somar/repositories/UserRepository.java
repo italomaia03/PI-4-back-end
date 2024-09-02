6a58643f29d92e62eb9044ecc18bdef75c9a94e1
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-09-01 21:59:12 -0300

    feat: add method to find user by UUID

--- src/main/java/br/edu/projeto_integrado/somar/repositories/UserRepository.java
+++ src/main/java/br/edu/projeto_integrado/somar/repositories/UserRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUuid(UUID uuid);
 }
