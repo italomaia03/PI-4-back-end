07e841e036c1561e1607625feeb6b323d63e791d
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-09-01 21:58:01 -0300

    feat: add product entity

---
+++ src/main/java/br/edu/projeto_integrado/somar/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.edu.projeto_integrado.somar.repositories;
+
+import br.edu.projeto_integrado.somar.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
