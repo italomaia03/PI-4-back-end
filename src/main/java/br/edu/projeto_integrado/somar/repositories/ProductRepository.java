0f2de9ee5581783fc236b69b3000769ff4faefc7
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-09-15 00:14:30 -0300

    feat: extend JpaSpecificationExecutor to manage products queries

--- src/main/java/br/edu/projeto_integrado/somar/repositories/ProductRepository.java
+++ src/main/java/br/edu/projeto_integrado/somar/repositories/ProductRepository.java
@@ -2,8 +2,9 @@ package br.edu.projeto_integrado.somar.repositories;
 
 import br.edu.projeto_integrado.somar.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
 }
