5ec053bcbac9955592b803f363d88e5f5a74cd42
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-09-16 00:08:14 -0300

    fix: clean database after each integration test to prevent conflicts

--- src/test/java/br/edu/projeto_integrado/somar/controllers/product/UpdateProductControllerIntegrationTest.java
+++ src/test/java/br/edu/projeto_integrado/somar/controllers/product/UpdateProductControllerIntegrationTest.java
@@ -9,12 +9,14 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -26,6 +28,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(UserManagementExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class UpdateProductControllerIntegrationTest {
 
     @Autowired
