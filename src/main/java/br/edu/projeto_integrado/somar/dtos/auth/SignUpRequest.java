7309970708971a2747cc493528e31b066beffdd1
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-09-17 23:43:19 -0300

    test: implement unit test for sign up service

--- src/main/java/br/edu/projeto_integrado/somar/dtos/auth/SignUpRequest.java
+++ src/main/java/br/edu/projeto_integrado/somar/dtos/auth/SignUpRequest.java
@@ -30,4 +30,20 @@ public class SignUpRequest {
     public String getLastName() {
         return lastName;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
